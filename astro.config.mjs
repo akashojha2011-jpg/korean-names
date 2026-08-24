f5d9524c751d71843d469d7c676168a2b5061e65
Akash Ojha <akashojha@192.168.1.4>
2026-08-24 21:01:15 +0530

    Set site domain to custom domain https://www.korean-names.info for Search Console

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: process.env.SITE_URL || 'https://korean-names.vercel.app',
+  site: 'https://www.korean-names.info',
   integrations: [sitemap()],
   output: 'static',
 });
