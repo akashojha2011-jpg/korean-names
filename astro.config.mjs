6802c558ae63af5d3103ef5c945cdcfc5d7c395c
Akash Ojha <akashojha@192.168.1.4>
2026-08-24 20:57:52 +0530

    Fix Search Console sitemap domain matching and add vercel.json rewrite & robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://korean-names.info',
+  site: process.env.SITE_URL || 'https://korean-names.vercel.app',
   integrations: [sitemap()],
   output: 'static',
 });
